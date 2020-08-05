6e0fde7e8a34849533c95dee15e015f581632a7a
Tony Yu <toekneeyou@tonys-mbp.lan>
2020-08-05 15:12:39 -0700

    button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ExploreHotel = React.lazy(() => import("app1/App"));
+const Button = React.lazy(() => import("app1/App"));
 
 export default class App extends React.Component {
   render() {
     return (
       <React.Suspense fallback="Loading app1">
-        <ExploreHotel />
+        <Button />
       </React.Suspense>
     );
   }
